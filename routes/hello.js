8e8cfd9eddbbed57c514f2f0af26d5ea91eafa4c
Happyholic1203 <Happyholic1203@gmail.com>
2015-01-15 14:46:43 +0800

    Change with param

--- routes/hello.js
+++ routes/hello.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.json({name: 'Henry'});
+    res.json({name: req.param('name'), method: 'GET'});
 });
 
 router.post('/', function(req, res) {
-    res.json({name: 'Henry'});
+    res.json({name: req.param('name'), method: 'POST'});
 });
 
 module.exports = router;
